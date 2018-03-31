0316fcf5e934426cbdd7529074ca4a92af808cfd
boa50 <bruno.albuquerque50@gmail.com>
2018-03-31 12:29:07 -0300

    Ajustes no repositório

--- app/src/main/java/br/com/boa50/kbingo/data/source/PedrasRepository.java
+++ app/src/main/java/br/com/boa50/kbingo/data/source/PedrasRepository.java
@@ -2,6 +2,7 @@ package br.com.boa50.kbingo.data.source;
 
 import com.google.common.collect.Lists;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -23,38 +24,24 @@ public class PedrasRepository implements PedrasDataSource {
     @Override
     public Flowable<List<Pedra>> getPedras() {
 
-        return Flowable.just(Lists.newArrayList(
-                new Pedra("1","K","01"),
-                new Pedra("2","K","02"),
-                new Pedra("3","K","03"),
-                new Pedra("4","K","04"),
-                new Pedra("5","K","05"),
-                new Pedra("6","K","06"),
-                new Pedra("7","K","07"),
-                new Pedra("8","K","08"),
-                new Pedra("9","K","09"),
-                new Pedra("10","K","10"),
-                new Pedra("11","K","11"),
-                new Pedra("12","K","12"),
-                new Pedra("13","K","13"),
-                new Pedra("14","K","14"),
-                new Pedra("15","K","15"),
-                new Pedra("1","I","01"),
-                new Pedra("2","I","02"),
-                new Pedra("3","I","03"),
-                new Pedra("4","I","04"),
-                new Pedra("5","I","05"),
-                new Pedra("6","I","06"),
-                new Pedra("7","I","07"),
-                new Pedra("8","I","08"),
-                new Pedra("9","I","09"),
-                new Pedra("10","I","10"),
-                new Pedra("11","I","11"),
-                new Pedra("12","I","12"),
-                new Pedra("13","I","13"),
-                new Pedra("14","I","14"),
-                new Pedra("15","I","15")
-                )
-        );
+        List<Pedra> pedras = new ArrayList<>();
+
+        String letra = "K";
+        for (int i = 1; i <= 75; i++) {
+            if (i == 16)
+                letra = "I";
+            else if (i == 31)
+                letra = "N";
+            else if (i == 46)
+                letra = "K";
+            else if (i == 61)
+                letra = "A";
+
+            String valorNumero = i < 10 ? "0" + Integer.toString(i) : Integer.toString(i);
+
+            pedras.add(new Pedra(Integer.toString(i),letra,valorNumero));
+        }
+
+        return Flowable.just(pedras);
     }
 }
