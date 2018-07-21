0abf2320a0311839881da88e8e5f170c48c1f28d
boa50 <bruno.albuquerque50@gmail.com>
2018-07-21 05:25:11 -0300

    Reorganização dos testes de interface

--- app/src/androidTest/java/br/com/boa50/kbingo/VisualizaCartelasEspressoTest.java
+++ app/src/androidTest/java/br/com/boa50/kbingo/VisualizaCartelasEspressoTest.java
@@ -1,32 +1,23 @@
 package br.com.boa50.kbingo;
 
-import android.arch.persistence.room.Room;
-import android.content.Context;
 import android.content.pm.ActivityInfo;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.contrib.DrawerActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.support.v4.view.GravityCompat;
-import android.view.Gravity;
 
-import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import br.com.boa50.kbingo.data.AppDataSource;
 import br.com.boa50.kbingo.data.AppDatabase;
-import br.com.boa50.kbingo.data.AppRepository;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
 import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.contrib.DrawerMatchers.isClosed;
-import static android.support.test.espresso.contrib.NavigationViewActions.navigateTo;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -37,32 +28,24 @@ import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
 public class VisualizaCartelasEspressoTest {
-    private AppDatabase db;
+    private static AppDatabase db;
 
     @Rule
     public ActivityTestRule<BaseActivity> mActivityRule =
             new ActivityTestRule<>(BaseActivity.class);
 
-    @Before
-    //TODO fazer alterações para o @BeforeClass para agilizar os testes
-    public void setup() {
-        Context context = InstrumentationRegistry.getTargetContext();
-        db = Room.databaseBuilder(context.getApplicationContext(),
-                AppDatabase.class, "Test.db").build();
-        AppDataSource appDataSource = new AppRepository(db);
-        appDataSource.initializeDatabase();
-
-        onView(withId(R.id.drawer_layout))
-                .check(matches(isClosed(Gravity.LEFT)))
-                .perform(DrawerActions.open());
-        onView(withId(R.id.navigation_view))
-                .perform(navigateTo(R.id.item_visualizar_cartelas));
-        onView(withId(R.id.drawer_layout))
-                .perform(DrawerActions.close());
+    @BeforeClass
+    public static void setup() {
+        db = CustomProcedures.initializeDatabase(db);
     }
 
-    @After
-    public void tearDown() {
+    @Before
+    public void setupTest() {
+        CustomProcedures.changeNavigation(R.id.item_visualizar_cartelas);
+    }
+
+    @AfterClass
+    public static void tearDown() {
         db.close();
     }
 
@@ -101,22 +84,8 @@ public class VisualizaCartelasEspressoTest {
         onView(withId(R.id.et_numero_cartela)).perform(replaceText("0002"));
         onView(withId(R.id.et_numero_cartela)).perform(pressImeActionButton());
 
-        onView(withId(R.id.drawer_layout))
-                .check(matches(isClosed(GravityCompat.START)))
-                .perform(DrawerActions.open());
-        onView(withId(R.id.navigation_view))
-                .perform(navigateTo(R.id.item_realizar_sorteio));
-
-        try {
-            Thread.sleep(400);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        onView(withId(R.id.drawer_layout))
-                .check(matches(isClosed(GravityCompat.START)))
-                .perform(DrawerActions.open());
-        onView(withId(R.id.navigation_view))
-                .perform(navigateTo(R.id.item_visualizar_cartelas));
+        CustomProcedures.changeNavigation(R.id.item_realizar_sorteio);
+        CustomProcedures.changeNavigation(R.id.item_visualizar_cartelas);
 
         onView(withId(R.id.et_numero_cartela)).check(matches(withText("0001")));
         onView(indexChildOf(withId(R.id.gl_cartela),5)).check(matches(withText(text)));
