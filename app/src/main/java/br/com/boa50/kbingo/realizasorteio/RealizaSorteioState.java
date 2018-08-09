cb4ded6c5473b6c0da332357aa37f64ff05cea28
boa50 <bruno.albuquerque50@gmail.com>
2018-08-09 17:19:36 -0300

    Inclusão das pedras no State do Presenter

--- app/src/main/java/br/com/boa50/kbingo/realizasorteio/RealizaSorteioState.java
+++ app/src/main/java/br/com/boa50/kbingo/realizasorteio/RealizaSorteioState.java
@@ -1,16 +1,25 @@
 package br.com.boa50.kbingo.realizasorteio;
 
+import java.util.ArrayList;
+
 import br.com.boa50.kbingo.data.entity.Pedra;
 
 public class RealizaSorteioState implements RealizaSorteioContract.State {
+    private final ArrayList<Pedra> mPedras;
     private final Pedra mUltimaPedraSorteada;
 
-    public RealizaSorteioState(Pedra ultimaPedraSorteada) {
+    public RealizaSorteioState(ArrayList<Pedra> pedras, Pedra ultimaPedraSorteada) {
+        mPedras = pedras;
         mUltimaPedraSorteada = ultimaPedraSorteada;
     }
 
     @Override
     public Pedra getUltimaPedraSorteada() {
         return mUltimaPedraSorteada;
+    }
+
+    @Override
+    public ArrayList<Pedra> getPedras() {
+        return mPedras;
     }
 }
