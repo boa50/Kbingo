427062fc4e3a347aa3e1c07c32fe7ec5f64bb9cd
boa50 <bruno.albuquerque50@gmail.com>
2018-08-11 17:42:23 -0300

    Criação de estrutura para conferir várias cartelas

---
+++ app/src/main/java/br/com/boa50/kbingo/conferecartelas/ConfereCartelasContract.java
@@ -0,0 +1,14 @@
+package br.com.boa50.kbingo.conferecartelas;
+
+import br.com.boa50.kbingo.BasePresenter;
+import br.com.boa50.kbingo.BaseView;
+
+public interface ConfereCartelasContract {
+    interface View extends BaseView {
+
+    }
+
+    interface Presenter extends BasePresenter<View> {
+
+    }
+}
---
+++ app/src/main/java/br/com/boa50/kbingo/conferecartelas/ConfereCartelasFragment.java
@@ -0,0 +1,8 @@
+package br.com.boa50.kbingo.conferecartelas;
+
+import br.com.boa50.kbingo.di.ActivityScoped;
+import dagger.android.support.DaggerFragment;
+
+@ActivityScoped
+public class ConfereCartelasFragment extends DaggerFragment implements ConfereCartelasContract.View {
+}
