441677b2c1d6533f9bd0acb2595c29ade65d940b
boa50 <bruno.albuquerque50@gmail.com>
2018-08-26 15:32:17 -0300

    Realizar sorteio dentre todas as cartelas disponíveis

--- app/src/main/java/br/com/boa50/kbingo/sorteiocartela/SorteioCartelaContract.java
+++ app/src/main/java/br/com/boa50/kbingo/sorteiocartela/SorteioCartelaContract.java
@@ -6,10 +6,10 @@ import br.com.boa50.kbingo.BaseView;
 public interface SorteioCartelaContract {
 
     interface View extends BaseView {
-
+        void apresentarCartela(String numeroCartela);
     }
 
     interface Presenter extends BasePresenter<View> {
-
+        void sortearCartela();
     }
 }
