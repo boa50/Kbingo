07ae7a63762cc03374da3362c1a684b611c54bb8
boa50 <bruno.albuquerque50@gmail.com>
2018-08-26 15:57:26 -0300

    Criação de testes

--- app/src/androidTest/java/br/com/boa50/kbingo/SorteioCartelaEspressoTest.java
+++ app/src/androidTest/java/br/com/boa50/kbingo/SorteioCartelaEspressoTest.java
@@ -1,4 +1,61 @@
 package br.com.boa50.kbingo;
 
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import br.com.boa50.kbingo.data.AppDatabase;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static br.com.boa50.kbingo.CustomGets.getButtonText;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+
+@RunWith(AndroidJUnit4.class)
 public class SorteioCartelaEspressoTest {
+    private static AppDatabase db;
+
+    @Rule
+    public ActivityTestRule<BaseActivity> mActivityRule =
+            new ActivityTestRule<>(BaseActivity.class);
+
+    @BeforeClass
+    public static void setup() {
+        db = CustomProcedures.initializeDatabase();
+    }
+
+    @Before
+    public void setupTest() {
+        CustomProcedures.changeNavigation(R.id.item_sorteio_cartela);
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        db.close();
+    }
+
+    @Test
+    public void realizarSorteio_mostrarNumeroCartela() {
+        onView(withId(R.id.bt_sorteio_cartela))
+                .check(matches(withText(R.string.bt_sorteio_cartela)));
+
+        onView(withId(R.id.bt_sorteio_cartela))
+                .perform(click());
+
+        onView(withId(R.id.bt_sorteio_cartela))
+                .check(matches(not(withText(R.string.bt_sorteio_cartela))));
+
+        assertThat(getButtonText(withId(R.id.bt_sorteio_cartela)).length(), equalTo(4));
+    }
 }
--- app/src/test/java/br/com/boa50/kbingo/sorteiocartela/SorteioCartelaPresenterTest.java
+++ app/src/test/java/br/com/boa50/kbingo/sorteiocartela/SorteioCartelaPresenterTest.java
@@ -1,4 +1,55 @@
 package br.com.boa50.kbingo.sorteiocartela;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+
+import br.com.boa50.kbingo.data.AppDataSource;
+import br.com.boa50.kbingo.util.schedulers.ImmediateScheduleProvider;
+import io.reactivex.Single;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
 public class SorteioCartelaPresenterTest {
+
+    @Mock
+    private SorteioCartelaContract.View view;
+    @Mock
+    private AppDataSource appDataSource;
+
+    private SorteioCartelaPresenter presenter;
+
+    @Before
+    public void setup() {
+        initMocks(this);
+        presenter = new SorteioCartelaPresenter(appDataSource, new ImmediateScheduleProvider());
+
+        when(appDataSource.getCartelaUltimoId()).thenReturn(Single.just(200));
+        presenter.subscribe(view);
+    }
+
+    @Test
+    public void sortearCartela_apresentarCartela() {
+        presenter.sortearCartela();
+
+        verify(view).apresentarCartela(anyString());
+    }
+
+    @Test
+    public void sortearCartela_retornarDentroDoRange() {
+        ArgumentCaptor<String> numCartela = ArgumentCaptor.forClass(String.class);
+        presenter.sortearCartela();
+
+        verify(view).apresentarCartela(numCartela.capture());
+
+        assertThat(Integer.valueOf(numCartela.getValue()), greaterThanOrEqualTo(1));
+        assertThat(Integer.valueOf(numCartela.getValue()), lessThanOrEqualTo(200));
+    }
 }
