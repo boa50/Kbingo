86d2c934b20e9288700fc0d8972061ff9519c485
boa50 <bruno.albuquerque50@gmail.com>
2018-08-30 04:49:52 -0300

    Criação de modelo para filtro de cartelas

--- app/src/main/java/br/com/boa50/kbingo/data/AppDataSource.java
+++ app/src/main/java/br/com/boa50/kbingo/data/AppDataSource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.inject.Singleton;
 
+import br.com.boa50.kbingo.data.dto.CartelaFiltroDTO;
 import br.com.boa50.kbingo.data.entity.CartelaPedra;
 import br.com.boa50.kbingo.data.entity.Letra;
 import br.com.boa50.kbingo.data.entity.Pedra;
@@ -12,11 +13,10 @@ import io.reactivex.Single;
 @Singleton
 public interface AppDataSource {
     Single<List<Letra>> getLetras();
-
     Single<List<Pedra>> getPedras();
-
     Single<Integer> getCartelaUltimoId();
     Single<List<CartelaPedra>> getPedrasByCartelaId(int id);
+    Single<List<CartelaFiltroDTO>> getCartelasFiltro();
 
     void initializeDatabase();
 }
--- app/src/main/java/br/com/boa50/kbingo/sorteiocartela/SorteioCartelaContract.java
+++ app/src/main/java/br/com/boa50/kbingo/sorteiocartela/SorteioCartelaContract.java
@@ -1,12 +1,16 @@
 package br.com.boa50.kbingo.sorteiocartela;
 
+import java.util.List;
+
 import br.com.boa50.kbingo.BasePresenter;
 import br.com.boa50.kbingo.BaseView;
+import br.com.boa50.kbingo.data.dto.CartelaFiltroDTO;
 
 public interface SorteioCartelaContract {
 
     interface View extends BaseView {
         void apresentarCartela(String numeroCartela);
+        void preencherCartelasFiltro(List<CartelaFiltroDTO> cartelasFiltro);
     }
 
     interface Presenter extends BasePresenter<View> {
