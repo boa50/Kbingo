2ff65ff401d8cf0d6834a7dcc9b9637266bc7dd4
boa50 <bruno.albuquerque50@gmail.com>
2018-11-15 11:15:41 -0200

    Ajustes para o Multidex

--- app/src/main/java/br/com/boa50/kbingo/KbingoApplication.java
+++ app/src/main/java/br/com/boa50/kbingo/KbingoApplication.java
@@ -19,6 +19,9 @@
 */
 package br.com.boa50.kbingo;
 
+import android.content.Context;
+import android.support.multidex.MultiDex;
+
 import br.com.boa50.kbingo.di.DaggerAppComponent;
 import dagger.android.AndroidInjector;
 import dagger.android.DaggerApplication;
@@ -27,5 +30,11 @@ public class KbingoApplication extends DaggerApplication {
     @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
         return DaggerAppComponent.builder().create(this);
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
     }
 }
